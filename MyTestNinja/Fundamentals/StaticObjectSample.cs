ecbcdbfe0f982f3f31c22c884cf954877f007090
TingYu_Wu <tingyu_wu@cmoney.com.tw>
2023-03-21 15:14:43 +0800

    add sample for AdditionInfoSample, StaticObjectSample, SecuritiesServiceSample.

---
+++ MyTestNinja/Fundamentals/StaticObjectSample.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+
+namespace MyTestNinja.Fundamentals
+{
+    internal static class Global
+    {
+        public static int X = 10;
+    }
+
+    public class StaticObjectSample
+    {
+        public int ComputeSomething()
+        {
+            if (Global.X == 10)
+            {
+                _ = Interlocked.Increment(ref Global.X);
+                return Global.X;
+            }
+
+            return Global.X;
+        }
+    }
+}
---
+++ MyTestNinja/Mocking/SecuritiesServiceSample.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyTestNinja.Mocking
+{
+    public class SecuritiesServiceSample
+    {
+        private PocketUser _user;
+
+        public OrderStatusPackets OrderStatus { get; set; }
+        public OrderUpdateResult OrderUpdate { get; set; }
+        public TradeUpdateResult TradeUpdate { get; set; }
+
+        public SecuritiesServiceSample()
+        {
+            _user = new PocketUser();
+            _user.OnReceivedOrderUpdate += OnReceivedOrderUpdate;
+            _user.OnReceivedTradeUpdate += OnReceivedTradeUpdate;
+        }
+
+        private void OnReceivedTradeUpdate(TradeUpdateResult ret)
+        {
+            TradeUpdate = ret;
+        }
+
+        private void OnReceivedOrderUpdate(OrderUpdateResult ret)
+        {
+            OrderUpdate = ret;
+        }
+
+        // by button trigger
+        private async void OnButtonClick(object sender, EventArgs e)
+        {
+            OrderStatus = await _user.EnterOrderAsync("2330", 1, 1);
+        }
+    }
+
+    //   -------------------------------------
+    public class PocketUser
+    {
+
+        public PocketUser()
+        {
+        }
+
+        public Action<OrderUpdateResult> OnReceivedOrderUpdate { get; set; }
+
+        public Action<TradeUpdateResult> OnReceivedTradeUpdate { get; set; }
+
+        private void ReceivedPackets()
+        {
+            OnReceivedOrderUpdate?.Invoke(new OrderUpdateResult());
+            OnReceivedTradeUpdate?.Invoke(new TradeUpdateResult());
+        }
+        public Task<OrderStatusPackets> EnterOrderAsync(string commodities
+            , int side
+            , int vol
+            , double? price = null)
+        {
+            return Task.FromResult(new OrderStatusPackets())
+                .ContinueWith(task =>
+                {
+                    ReceivedPackets();
+                    return task.Result;
+                });
+        }
+    }
+
+    public class OrderStatusPackets
+    {
+    }
+
+    public class PacketEventArgs : EventArgs
+    {
+        public PacketEventArgs(IReadOnlyDictionary<string, object> packets)
+        {
+            Packets = packets;
+        }
+
+        public IReadOnlyDictionary<string, object> Packets { get; }
+
+    }
+
+    public class TradeUpdateResult
+    {
+    }
+
+    public class OrderUpdateResult
+    {
+    }
+
+    public interface ISecuritiesConnection
+    {
+    }
+    
+}
